b7cf0724c0d93a2ab105f4a190f9f478481354d0
IKEA-luddes <user@DESKTOP-DGOC8RS.localdomain>
2022-03-01 01:38:29 +0100

    [Feat✨] (GameEngine) : Update scene.c

--- lib/game_engine/scene/scene.c
+++ lib/game_engine/scene/scene.c
@@ -7,7 +7,30 @@
 
 #include "game_engine.h"
 
+static int cmp(char const *s1, char const *s2)
+{
+    int i = 0;
+    int result = 0;
+
+    while (s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i])
+        i++;
+    result = s1[i] - s2[i];
+    return (result);
+}
+
 sfBool change_scene(char const *name, engine_t *engine)
 {
-    
+    node_t *current = NULL;
+
+    if (engine == NULL)
+        return NULL;
+    current = engine->scenes->head;
+    for (int i = 0; i < engine->scenes->nb_elements; i++) {
+        if (cmp(name, current->key)) {
+            engine->actual_scene = i;
+            return sfTrue;
+        }
+        current = current->next;
+    }
+    return sfFalse;
 }
