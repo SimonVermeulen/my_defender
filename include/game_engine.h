61b76c1985ad4fe32f5d88170b64f6e25eb55136
IKEA-luddes <user@DESKTOP-DGOC8RS.localdomain>
2022-03-01 01:36:39 +0100

    [Feat✨] (GameEngine) : Update game_engine.h

--- include/game_engine.h
+++ include/game_engine.h
@@ -64,26 +64,44 @@ typedef struct addon_s {
     event_functions_t on_scene_loaded;
 } addon_t;
 
-engine_t *init_game(sfVideoMode *video, char const *title);
+engine_t *init_game(sfVideoMode video, char const *title);
 int open_game(engine_t *engine, int fps);
-sfBool change_scene(char const *name, engine_t *engine);
 int destroy_game(engine_t *engine);
-int destroy_print_list(engine_t *engine);
+
 int destroy_functions(engine_t *engine);
+
+void *get_addon(char const *name, object_t *object);
+sfBool add_addon(char const *name, object_t *object, engine_t *engine);
 int destroy_addons(list_t *addon);
+
+typedef struct print_text_s {
+    sfFont *font;
+    char const *text;
+    sfVector2f position;
+} print_text_t;
+
+int print_list(engine_t *engine);
+int destroy_print_list(engine_t *engine);
+
 int init_text(engine_t *engine);
 sfBool add_font(char const *font, engine_t *engine);
 sfBool print_text(char const *text, sfVector2f position, engine_t *engine);
-sfBool draw_text(char const *text, sfVector2f position, engine_t *engine);
+sfBool draw_text(print_text_t *data, engine_t *engine);
 int destroy_text(engine_t *engine);
+
 int init_scene(char const *name, sfBool const_scene, engine_t *engine);
+sfBool change_scene(char const *name, engine_t *engine);
 int init_scene_by_list(list_t *object, sfBool const_scene, engine_t *engine);
 int destroy_scene(scene_t *scene, engine_t *engine);
+
 int create_object(char const *name, engine_t *engine);
 int create_object_by_list(list_t *object, engine_t *engine);
 int destroy_object(object_t *object, engine_t *engine);
+
 int init_entity(char const *texture, object_t *object);
+int print_entity(object_t *object);
 int destroy_entity(object_t *object);
+
 sfBool set_active(sfBool value, object_t *object, engine_t *engine);
 sfBool add_function(event_functions_t functions, float time);
 float get_delta(engine_t *engine);
