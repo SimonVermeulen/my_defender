b09148103a33bc9095cb9c2f7532e92f327bc6eb
simon.vermeulen <simon.vermeulen@epitech.eu>
2022-03-08 03:32:33 +0100

    [Feat✨](Linked_lists) : creation of splice function

--- lib/linked_list/add_delete_node.c
+++ lib/linked_list/add_delete_node.c
@@ -25,6 +25,20 @@ node_t *create_newnode(void)
     return (new_node);
 }
 
+void splice(list_t *list, int index)
+{
+    node_t *current_node = NULL;
+
+    for (int i = 0; i < index; i++)
+        current_node = current_node->next;
+    if (current_node == list->head)
+        current_node->next = list->head;
+    current_node->prev->next = current_node->next;
+    current_node->next->prev = current_node->prev;
+    free_node(current_node);
+    list->nb_elements--;
+}
+
 //à coder
 static void free_value(node_t *node);
 
