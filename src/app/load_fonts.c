c80f01afb68052b2e34008e160ea7af907a5c904
IKEA.kivik <kivik@ikea.eu>
2022-03-13 19:33:16 +0100

    [Feat✨] (MainMenu) : update MainMenu Add background and logo

--- src/app/load_fonts.c
+++ src/app/load_fonts.c
@@ -7,12 +7,12 @@
 
 #include "game.h"
 
-const char fonts_path[][256] = {NULL};
-const char fonts_name[][256] = {NULL};
+const char fonts_path[][256] = {"\0"};
+const char fonts_name[][256] = {"\0"};
 
 int load_fonts(engine_t *engine)
 {
-    for (int i = 0; fonts_name[i] != NULL; i++) {
+    for (int i = 0; fonts_name[i][0] != 0; i++) {
         if (add_font(fonts_path[i], fonts_name[i], engine) == sfFalse)
             return 84;
     }
