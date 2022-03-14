0e8d90f1d2d4bf4a35b7f619345c345c7e5a5b8e
Nouiri Adil <adil.nouiri@epitech.eu>
2022-03-14 10:54:36 +0100

    Create Random branch and randomize function

---
+++ include/random.h
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2021
+** B-CPE-100-TLS-1-1-cpoolday09-younes1.bahri
+** File description:
+** my.h
+*/
+
+#ifndef RANDOM_H
+    #define RANDOM_H
+    #include <stdio.h>
+
+int randomize(void);
+
+int randomize_helper(FILE *in);
+
+#endif
