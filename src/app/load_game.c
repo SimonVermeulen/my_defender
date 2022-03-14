4b618a8c97932beefc1872b3ed981f30faac178c
IKEA.kivik <kivik@ikea.eu>
2022-03-14 01:25:32 +0100

    [Feat✨] (GameEngine) : add position addon

--- src/app/load_game.c
+++ src/app/load_game.c
@@ -8,9 +8,9 @@
 #include "game.h"
 
 const int (*load_scene[])(engine_t *) = {init_main_menu, NULL};
-const int (*load_addon[])(engine_t *) = {NULL};
+const int (*load_addon[])(engine_t *) = {init_position_addons, NULL};
 
-const int width = 1980;
+const int width = 1920;
 const int height = 1080;
 const int fps = 60;
 const char title[] = "titre";
@@ -22,6 +22,10 @@ int load_game(void)
     if (engine == NULL || load_addons(engine, load_addon) == 84 ||
         load_scenes(engine, load_scene) == 84 || load_fonts(engine) == 84)
         return 84;
+    sfView_setCenter(engine->view, (sfVector2f) {(width / 2), (height / 2)});
+    sfView_setSize(engine->view, (sfVector2f) {width, height});
+    sfView_zoom(engine->view, 0.98);
+    sfRenderWindow_setView(engine->window, engine->view);
     change_scene("MainMenu", engine);
     return open_game(engine, fps);
 }
