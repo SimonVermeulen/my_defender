3cf3fd2bbbde30958e3bdb6b4e5f418b55434f40
IKEA-luddes <user@DESKTOP-DGOC8RS.localdomain>
2022-03-15 20:43:11 +0100

    [Feat✨] (Game) : add valid_build addon

--- src/app/addons/builder_selector/tower_selector.c
+++ src/app/addons/builder_selector/tower_selector.c
@@ -10,16 +10,18 @@
 int on_click_tower_a(object_t *object, engine_t *engine, sfFloatRect rect,
     sfVector2f mouse)
 {
-    sfFloatRect button = (sfFloatRect) {rect.left + 7, rect.top + 12, 30, 30};
-    static sfBool is_valid = sfFalse;
+    sfFloatRect button = (sfFloatRect) {rect.left, rect.top + 3, 50, 50};
     node_t *tower = search_from_key(get_addon("Tower", 0, object),
         "TowerArcher");
+    object_t *valid = seach_object(engine, "ValidBuild");
 
-    if (sfFloatRect_contains(&button, mouse.x, mouse.y) == sfTrue &&
-        is_valid == sfFalse) {
-        printf("je passes\n");
-        is_valid = sfTrue;
+    if (valid == NULL)
+        return 0;
+    if (sfFloatRect_contains(&button, mouse.x, mouse.y) == sfTrue) {
         set_active(sfTrue, tower->value, engine);
+        sfSprite_setPosition(valid->entity->sprite, (sfVector2f)
+            {button.left, button.top});
+        set_active(sfTrue, valid, engine);
     }
     return 0;
 }
