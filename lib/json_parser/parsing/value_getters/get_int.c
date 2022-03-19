b8b5fd2660138ee281e23a286da95111d1726d2a
simon.vermeulen <simon.vermeulen@epitech.eu>
2022-03-19 16:16:18 +0100

    [Fix 🧩] (Linked_list) minor errors and bugs

--- lib/json_parser/parsing/value_getters/get_int.c
+++ lib/json_parser/parsing/value_getters/get_int.c
@@ -2,7 +2,7 @@
 ** EPITECH PROJECT, 2022
 ** json_parser2.0
 ** File description:
-** gets a double from the given buff
+** gets an int from the given buff
 */
 
 #include <stdlib.h>
@@ -11,21 +11,21 @@
 #include "linked_list.h"
 #include "json_parser.h"
 
-static int get_nb(const char *buff, int index, double *result)
+static int get_nb(const char *buff, int index, int *array)
 {
     int len = 0;
 
-    for (; buff[len] && is_number(buff[len]) || buff[len] == '.'; len++);
-    result[index] = atof(buff);
+    for (; buff[len] && is_number(buff[len]); len++);
+    array[index] = atoi(buff);
     return (len);
 }
 
-static int get_double_array(const char *buff, int nb_elements, double *result)
+static int get_int_array(const char *buff, int nb_elements, int *array)
 {
     int len = 0;
 
     for (int i = 0; i < nb_elements; i++) {
-        len += get_nb(&buff[len], i, result);
+        len += get_nb(&buff[len], i, array);
         if (buff[len] && buff[len] != ',' && i != nb_elements - 1) {
             write(2, "Error: expected a ','\n", 23);
             return (-1);
@@ -36,19 +36,20 @@ static int get_double_array(const char *buff, int nb_elements, double *result)
     return (len);
 }
 
-int get_double(const char *buff, node_t *new_node, int nb_elements)
+int get_int(const char *buff, node_t *new_node, int nb_elements)
 {
     int len = 0;
-    double *result = NULL;
+    int *result = NULL;
 
     new_node->len = (nb_elements == 0) ? 1 : nb_elements;
-    result = malloc(sizeof(double) * (new_node->len));
+    result = malloc(sizeof(int) * (new_node->len));
     if (!result)
         return (-1);
     if (nb_elements == 0)
         len = get_nb(buff, 0, result);
     else
-        len = get_double_array(buff, nb_elements, result);
+        len = get_int_array(buff, nb_elements, result) + 1;
     new_node->value = result;
+    new_node->len = (nb_elements == 0) ? 0 : nb_elements;
     return (len);
 }
