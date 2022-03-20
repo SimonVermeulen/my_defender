56a950b9c0a1bde45b3a88c36052f076905f4dfe
IKEA-luddes <user@DESKTOP-DGOC8RS.localdomain>
2022-03-20 15:18:20 +0100

    [Style 🩹] (dev) : repear norme print_ghost

--- src/app/addons/tower/build_selector/build_selector.c
+++ src/app/addons/tower/build_selector/build_selector.c
@@ -7,6 +7,9 @@
 
 #include "game.h"
 
+int print_ghost(list_t *tower, object_t *object, engine_t *engine,
+    sfFloatRect rect);
+
 int start_build_selector(object_t *object, engine_t *engine)
 {
     object_t *valid = seek_object_scene(object->childs, "Valid");
@@ -20,36 +23,6 @@ int start_build_selector(object_t *object, engine_t *engine)
     set_active(sfFalse, valid, engine);
     set_active(sfFalse, object, engine);
     set_active(sfFalse, sprite, engine);
-    return 0;
-}
-
-int print_ghost(list_t *tower, object_t *object, engine_t *engine,
-    sfFloatRect rect)
-{
-    object_t *valid = seek_object_scene(object->childs, "Valid");
-    object_t *sprite = seek_object_scene(object->actual_scene, "SpriteTower");
-    double *position_x = get_value_list(tower, "PositionX", 2);
-    double *position_y = get_value_list(tower, "PositionY", 2);
-    double *x = get_addon("PositionX", 2, valid);
-    double *y = get_addon("PositionY", 2, valid);
-    char *path = get_value_list(tower, "Path", 4);
-    int *type = get_value_list(tower, "type", 3);
-    int *type_parent = get_addon("type", 3, object->parent);
-    sfVector2f mouse = get_mouse_position(engine);
-
-    if (!position_x || !position_y || !path || !type_parent)
-        return exit_game(engine, 84);
-    rect.left = rect.left + *position_x;
-    rect.top = rect.top + *position_y;
-    if (sfFloatRect_contains(&rect, mouse.x, mouse.y)) {
-        set_active(sfTrue, valid, engine);
-        set_active(sfTrue, sprite, engine);
-        *x = rect.left;
-        *y = rect.top;
-        *type_parent = *type;
-        if (change_texture(sprite, path) == 84)
-            return exit_game(engine, 84);
-    }
     return 0;
 }
 
